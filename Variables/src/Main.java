9b4c9ab32269580ce7fd6ff14157f0a5d1a8eab5
Jacksxn <loadconfigmc@gmail.com>
2018-10-15 15:58:58 -0700

    Variables Hand In

---
+++ Variables/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String args[]){
+
+        System.out.println("#########Spongebob by: Jackson Frazer########");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("     |¯¯--__--¯¯--__--¯¯--__--¯¯--__--¯¯|     ");
+        System.out.println("      |                                 |      ");
+        System.out.println("      |                                 |      ");
+        System.out.println("     |     O   ＼ | ／   ＼ | ／      o  |     ");
+        System.out.println("      |   o     _-¯-_    _-¯-_         |      ");
+        System.out.println("      |        |  0  |  |  0  |        |      ");
+        System.out.println("     |          ¯-_-¯ __ ¯-_-¯          |     ");
+        System.out.println("     |               |  |               |     ");
+        System.out.println("      |                ／              |      ");
+        System.out.println("      |      >--__          __--<      |      ");
+        System.out.println("   ／|             ¯¯▮¯▮¯¯             |＼    ");
+        System.out.println(" ／  |               ~~~~           O   |  ＼  ");
+        System.out.println(" |  | |   o                       o    | |  | ");
+        System.out.println(" |__| |    O                           | |__| ");
+        System.out.println("  |  |__--¯¯--__--¯¯--__--¯¯--__--¯¯--__| |   ");
+        System.out.println("  |   |          ＼／＼_／＼／           |  |   ");
+        System.out.println("  |   |______________／ ＼______________|  |   ");
+        System.out.println("  |   |  __   __    |    |    __   __  |  |   ");
+        System.out.println("  |   |              ＼_／              |  |   ");
+        System.out.println("  |   |________________________________|  |   ");
+        System.out.println("／|＼        |   |             |   |     ／|＼ ");
+        System.out.println("| | |        ¯¯¯¯              ¯¯¯¯     | | | ");
+        System.out.println("              ||                ||            ");
+        System.out.println("              ||                ||            ");
+        System.out.println("              ▮                ▮            ");
+        System.out.println("              ▮                ▮            ");
+        System.out.println("              ▮                ▮            ");
+        System.out.println("              ▮                ▮            ");
+        System.out.println("              ▮                ▮            ");
+        System.out.println("         _ _-¯¯-_            _-¯¯-_ _        ");
+        System.out.println("        ◖       |          |        ◗       ");
+        System.out.println("         ¯¯¯¯¯¯¯¯           ¯¯¯¯¯¯¯¯¯        ");
+        System.out.println("");
+        System.out.println("");
+
+    }
+
+}
